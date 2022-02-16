b401569cc37faaacd643180211ab5d573453ac63
Tasya Setyo Harwati <tasya.setyo@ti.ukdw.ac.id>
2022-02-16 12:29:49 +0700

    commit pertama

---
+++ Cobagit.java
@@ -0,0 +1,16 @@
+
+public class Contoh<NIM> {
+ public static void main(String args[]) {
+ int x=10;
+ int y=25;
+ int z=x+y;
+
+7 System.out.println("Hasil penjumlahan"
+8 +" dari x+y = " + z);
+9 System.out.println("Hasil akar kuadrat "
+10 +"dari z = " + Math.sqrt(z));
+11
+12 String namadepan = "Antonius";
+13 System.out.println("Nama depan saya adalah = " + namadepan);
+14 System.out.println("Nama lengkap saya "
+15 +"adalah = " + namadepan + " Rachmat");
